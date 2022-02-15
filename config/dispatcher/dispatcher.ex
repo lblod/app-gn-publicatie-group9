c556a2394d4e41a4efe008b4e4d44a4f82349e4b
Niels Vandekeybus <progster@gmail.com>
2022-02-15 09:53:32 +0100

    Revert "use cache where possible"
    This reverts commit a2789e45a705977c59825908b8e4fcc62cc9e9b7.

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -12,31 +12,31 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://published-resource-consumer/"
   end
   match "/agendas/*path", @any do
-    Proxy.forward conn, path, "http://cache/agendas/"
+    Proxy.forward conn, path, "http://resource/agendas/"
   end
 
   match "/besluitenlijsten/*path", @any do
-    Proxy.forward conn, path, "http://cache/besluitenlijsten/"
+    Proxy.forward conn, path, "http://resource/besluitenlijsten/"
   end
 
   match "/uittreksels/*path", @any do
-    Proxy.forward conn, path, "http://cache/uittreksels/"
+    Proxy.forward conn, path, "http://resource/uittreksels/"
   end
 
   match "/agendapunten/*path", @any do
-    Proxy.forward conn, path, "http://cache/agendapunten/"
+    Proxy.forward conn, path, "http://resource/agendapunten/"
   end
 
   match "/behandelingen-van-agendapunten/*path", @any do
-    Proxy.forward conn, path, "http://cache/behandelingen-van-agendapunten/"
+    Proxy.forward conn, path, "http://resource/behandelingen-van-agendapunten/"
   end
 
   match "/stemmingen/*path", @any do
-    Proxy.forward conn, path, "http://cache/stemmingen/"
+    Proxy.forward conn, path, "http://resource/stemmingen/"
   end
 
   match "/besluiten/*path", @any do
-    Proxy.forward conn, path, "http://cache/besluiten/"
+    Proxy.forward conn, path, "http://resource/besluiten/"
   end
 
   match "/bestuurseenheden/*path", @any do
@@ -49,7 +49,7 @@ defmodule Dispatcher do
 
   match "/bestuurseenheid-classificatie-codes/*path", @any do
     IO.inspect(conn, label: "conn to cache")
-    Proxy.forward conn, path, "http://cache/bestuurseenheid-classificatie-codes/"
+    Proxy.forward conn, path, "http://resource/bestuurseenheid-classificatie-codes/"
   end
 
   match "/bestuursorganen/*path", @any do
@@ -61,31 +61,36 @@ defmodule Dispatcher do
   end
 
   match "/zittingen/*path", @any do
-    Proxy.forward conn, path, "http://cache/zittingen/"
+    Proxy.forward conn, path, "http://resource/zittingen/"
   end
 
   match "/notulen/*path", @any do
-    Proxy.forward conn, path, "http://cache/notulen/"
+    Proxy.forward conn, path, "http://resource/notulen/"
+  end
+
+
+  match "/signed-resources/*path", @any do
+    Proxy.forward conn, path, "http://resource/signed-resources/"
   end
 
   match "/published-resources/*path", @any do
-    Proxy.forward conn, path, "http://cache/published-resources/"
+    Proxy.forward conn, path, "http://resource/published-resources/"
   end
 
   match "/versioned-agendas/*path", @any do
-    Proxy.forward conn, path, "http://cache/versioned-agendas/"
+    Proxy.forward conn, path, "http://resource/versioned-agendas/"
   end
 
   match "/versioned-besluiten-lijsten/*path", @any do
-    Proxy.forward conn, path, "http://cache/versioned-besluiten-lijsten/"
+    Proxy.forward conn, path, "http://resource/versioned-besluiten-lijsten/"
   end
 
   match "/versioned-behandelingen/*path", @any do
-    Proxy.forward conn, path, "http://cache/versioned-behandelingen/"
+    Proxy.forward conn, path, "http://resource/versioned-behandelingen/"
   end
 
   match "/versioned-notulen/*path", @any do
-    Proxy.forward conn, path, "http://cache/versioned-notulen/"
+    Proxy.forward conn, path, "http://resource/versioned-notulen/"
   end
 
   match "/concepts/*path", @any do
